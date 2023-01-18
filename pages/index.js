bb695e92e714f48b10e9b86739cdbd68511a1e4f
Muden <mudennn@yahoo.com>
2023-01-18 23:45:16 +0800

    update form

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ import {
 import Link from "next/link";
 
 export default function Home() {
-  const form = useRef();
+  const Form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -40,7 +40,7 @@ export default function Home() {
       .sendForm(
         "service_fffl9yr",
         "template_3x6hyf4",
-        form.current,
+        Form.current,
         "JpKGWHez2QnKag4MD"
       )
       .then(
@@ -521,7 +521,7 @@ export default function Home() {
               Ask me anything!
             </p>
           </div>
-          <form ref={form} onSubmit={sendEmail}>
+          <form ref={Form} onSubmit={sendEmail}>
             <div className="-mx-2 md:items-center md:flex">
               <div className="flex-1 px-2">
                 <label className="block mb-2 text-sm">Full Name</label>
